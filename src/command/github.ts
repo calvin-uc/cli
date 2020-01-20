30840a5f2b0ec356b669e723d0ed0dd76b5885e5
calvin.lee <calvin.lee@compass.com>
2020-01-20 16:57:24 -0500

    commented out code that wont work

--- src/command/github.ts
+++ src/command/github.ts
@@ -1,15 +1,15 @@
-import getGithubApp from './lib/getGithubApp';
+// import getGithubApp from './lib/getGithubApp';
 
-const GITHUB_OWNER = 'UrbanCompass';
+// const GITHUB_OWNER = 'UrbanCompass';
 
-export default async function getRateLimit(
-  org = GITHUB_OWNER,
-  pages = 100,
-) {
-  const github = await getGithubApp();
+// export default async function getRateLimit(
+//   org = GITHUB_OWNER,
+//   pages = 100,
+// ) {
+//   const github = await getGithubApp();
 
-  const resp = await github.rateLimit.get();
-  console.log(resp.data);
-}
+//   const resp = await github.rateLimit.get();
+//   console.log(resp.data);
+// }
 
-getRateLimit();
+// getRateLimit();
