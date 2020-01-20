ed9d6cfaac26d117ecfa68c0273771a6e9d1fc8b
calvin.lee <calvin.lee@compass.com>
2020-01-20 16:58:19 -0500

    pass in cwd to install

--- src/bin/ucfe.ts
+++ src/bin/ucfe.ts
@@ -1,6 +1,9 @@
 import {Command} from 'commander';
 
-import {execute as handleInstallAction} from '../command/install';
+import {
+  execute as handleInstallAction,
+  Options as InstallOptions,
+} from '../command/install';
 
 export async function cli(argv: string[]): Promise<void> {
   const program = new Command();
@@ -9,7 +12,10 @@ export async function cli(argv: string[]): Promise<void> {
     .description('install packages in category (apps|packages)/name')
     .option('--regen', 'regenerate lock file', false)
     .option('--clean', 'remove node modules and store', false)
-    .action(handleInstallAction);
+    .action((category: string, name: string, opts: InstallOptions) => {
+      opts.cwd = process.cwd();
+      return handleInstallAction(category, name, opts);
+    });
 
   program.parse(argv);
 }
